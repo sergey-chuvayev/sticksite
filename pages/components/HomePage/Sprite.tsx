cb6a8fddde792cb0e63b6a17edb10189a0de0c53
Sergey <sergey@spendesk.com>
2020-04-23 01:23:35 +0200

    --amend

--- pages/components/HomePage/Sprite.tsx
+++ pages/components/HomePage/Sprite.tsx
@@ -4,14 +4,19 @@ import SpriteAnimator from 'react-sprite-animator';
 const Sprite = () => {
  return (
     <SpriteAnimator
-      sprite='/sprite1.png'
-      width={540}
-      height={420}
-      fps={1}
+      sprite='/sprite.png'
+      width={460}
+      height={312}
+      fps={30}
       direction="vertical"
-      frameCount={87}
-      wrapAfter={9}
+      frameCount={88}
+      wrapAfter={30}
     />
+    // <div style={{
+    //   backgroundImage: 'url(/sprite.png)',
+    //   width: 540,
+    //   height: 420,
+    // }}></div>
  )
 }
 
