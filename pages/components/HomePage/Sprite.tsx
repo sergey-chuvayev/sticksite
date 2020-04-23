27ff7e8ff4bfdc804e9efe8cec44b48cc294a403
Sergey <sergey@spendesk.com>
2020-04-23 02:05:03 +0200

    --amend

--- pages/components/HomePage/Sprite.tsx
+++ pages/components/HomePage/Sprite.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import SpriteAnimator from 'react-sprite-animator';
+import React from "react";
+import SpriteAnimator from "react-sprite-animator";
+import style from "./HomePage.module.scss";
 
 const Sprite = () => {
- return (
-    <SpriteAnimator
-      sprite='/sprite.png'
-      width={460}
-      height={312}
-      fps={40}
-      direction="vertical"
-      frameCount={88}
-      wrapAfter={9}
-      loop={false}
-    />
- )
-}
+  return (
+    <div className={style['Sprite']}>
+      <SpriteAnimator
+        sprite="/sprite.png"
+        width={460}
+        height={312}
+        fps={40}
+        direction="vertical"
+        frameCount={88}
+        wrapAfter={9}
+        loop={false}
+      />
+    </div>
+  );
+};
 
 export default Sprite;
