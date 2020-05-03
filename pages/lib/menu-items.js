1fdedeee3e0f8d06f78a9e13adb6bc857cfb9e43
Sergey <sergey@spendesk.com>
2020-05-03 10:16:55 +0200

    fix

---
+++ pages/lib/menu-items.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+export const menuItems = [
+  {
+    id: 'auto',
+    icon: '/menu/auto.svg',
+    photo: '/menu/auto.png',
+    subtext: (
+      <span>Auto</span>
+    ),
+  },
+  {
+    id: 'home',
+    icon: '/menu/home.svg',
+    photo: '/menu/home.png',
+    subtext: (
+      <>
+        <span>HOME & </span>
+        <br />
+        <span>OFFICE</span>
+      </>
+    ),
+  },
+  {
+    id: 'sport',
+    icon: '/menu/sport.svg',
+    photo: '/menu/sport.png',
+    subtext: (
+      <span>Sport</span>
+    ),
+  },
+  {
+    id: 'travel',
+    icon: '/menu/travel.svg',
+    photo: '/menu/travel.png',
+    subtext: (
+      <span>Travel</span>
+    ),
+  },
+  {
+    id: 'music',
+    icon: '/menu/music.svg',
+    photo: '/menu/music.png',
+    subtext: (
+      <span>Music</span>
+    ),
+  },
+  {
+    id: 'blogging',
+    icon: '/menu/blogging.svg',
+    photo: '/menu/blogging.png',
+    subtext: (
+      <span>Blogging</span>
+    ),
+  },
+  {
+    id: 'horeca',
+    icon: '/menu/horeca.svg',
+    photo: '/menu/horeca.png',
+    subtext: (
+      <span>horeca</span>
+    ),
+  },
+  {
+    id: 'phone',
+    icon: '/menu/phone.svg',
+    photo: '/menu/phone.png',
+    subtext: (
+      <span>No phone</span>
+    ),
+  },
+]
